e2f23a2e71b346a91e8bb2854c61a06f03478ae1
liujane <1171603265@qq.com>
2019-01-25 00:25:04 +0800

    前端静态页面

---
+++ frontend/src/test/java/com/cross2u/frontend/FrontendApplicationTests.java
@@ -0,0 +1,17 @@
+package com.cross2u.frontend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FrontendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
