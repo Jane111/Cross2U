6f02f199648c7d1eddc96be3dd958563c269d49f
liujane <1171603265@qq.com>
2019-02-09 15:12:17 +0800

    上传部分文件

--- indent/src/main/java/com/cross2u/indent/IndentApplication.java
+++ indent/src/main/java/com/cross2u/indent/IndentApplication.java
@@ -2,6 +2,11 @@ package com.cross2u.indent;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
 
 @SpringBootApplication
 public class IndentApplication {
