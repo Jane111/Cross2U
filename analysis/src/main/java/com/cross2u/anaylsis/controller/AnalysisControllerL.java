a73d34f66039a98228005606d17b3bcb79c9005d
liujane <1171603265@qq.com>
2019-03-01 14:11:04 +0800

    提交代码保存-刘静2.28

---
+++ analysis/src/main/java/com/cross2u/anaylsis/controller/AnalysisControllerL.java
@@ -0,0 +1,16 @@
+package com.cross2u.anaylsis.controller;
+
+import com.cross2u.anaylsis.util.BaseItemRecommender;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigInteger;
+import java.util.List;
+@RestController
+@RequestMapping("/analysis")
+public class AnalysisControllerL {
+
+
+
+}
