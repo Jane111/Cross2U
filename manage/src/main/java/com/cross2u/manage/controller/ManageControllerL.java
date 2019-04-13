071046bd6c35a9d25f1d2624e94063414179ad28
liujane <1171603265@qq.com>
2019-04-13 15:52:31 +0800

    新添加的类

---
+++ manage/src/main/java/com/cross2u/manage/controller/ManageControllerL.java
@@ -0,0 +1,10 @@
+package com.cross2u.manage.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/manage")
+public class ManageControllerL {
+
+}
